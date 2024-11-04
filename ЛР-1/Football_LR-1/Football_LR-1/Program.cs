b36aa518538c22bde3bcdd576a865b7ac996ead7
Gavrilchenko_Stepan <@DESKTOP-DHH24NC>
2024-11-04 20:47:58 +0300

    Создал пустой проект для Лабораторной работы №1

---
+++ ЛР-1/Football_LR-1/Football_LR-1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Football_LR_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
