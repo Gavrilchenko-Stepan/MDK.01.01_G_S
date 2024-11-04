10864ff99a5daa8ce07a892d6e111a0b297064a1
Gavrilchenko_Stepan <@DESKTOP-DHH24NC>
2024-11-04 21:00:56 +0300

    Добавил новую стуктуру Footballer, задал перемнные и установил им значения

--- ЛР-1/Football_LR-1/Football_LR-1/Program.cs
+++ ЛР-1/Football_LR-1/Football_LR-1/Program.cs
@@ -6,6 +6,17 @@ using System.Threading.Tasks;
 
 namespace Football_LR_1
 {
+    struct Footballer
+    {
+        public string Name { get; set; }
+        public int Goals { get; set; }
+
+        public Footballer(string name, int goals)
+        {
+            Name = name;
+            Goals = goals;
+        }
+    }
     internal class Program
     {
         static void Main(string[] args)
