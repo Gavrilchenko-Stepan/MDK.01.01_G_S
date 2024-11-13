6ab3a52a9c959afa49a7fddb6efc04a5b05d94de
Gavrilchenko_Stepan <@DESKTOP-DHH24NC>
2024-11-13 19:28:40 +0300

    Добавил в класс Sale два конструктора

--- ЛР-2/Бытовая техника/Sale.cs
+++ ЛР-2/Бытовая техника/Sale.cs
@@ -8,5 +8,23 @@ namespace Бытовая_техника
 {
     internal class Sale
     {
+        private int check_;
+        private Appliances appliances_;
+        private double count_;
+        private DateTime date_;
+
+        public Sale(int check)
+        {
+            check_ = check;
+        }
+
+        public Sale SetSale(Appliances appliances, double count)
+        {
+            date_ = DateTime.Now;
+            appliances_ = appliances;
+            count_ = count;
+
+            return this;
+        }
     }
 }
