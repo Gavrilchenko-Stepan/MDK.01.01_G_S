b633fe6d2d48458130f040746e0dd48410c56d70
Gavrilchenko_Stepan <@DESKTOP-DHH24NC>
2024-11-13 18:54:17 +0300

    Создал в репозитории папку ЛР-2 Начал доработку ЛР №2

---
+++ ЛР-2/Бытовая техника/bettec.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Бытовая_техника
+{
+    public enum betovay_tecnica
+    {
+        fridges =1,
+        microwaves,
+        vacuumcleaners,
+        washingmacchnes
+    }
+}
