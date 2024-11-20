5d908c3caa0c36da4e4fdf7dca1444cb976715fb
Gavrilchenko_Stepan <@DESKTOP-DHH24NC>
2024-11-20 21:11:32 +0300

    Доработал класс SaleHistory: добавил список Sales, метод для добавления объекта в этот список и добавил вывод истории продаж

--- ЛР-2/Бытовая техника/SaleHistory.cs
+++ ЛР-2/Бытовая техника/SaleHistory.cs
@@ -8,11 +8,42 @@ namespace Бытовая_техника
 {
     class SaleHistory
     {
-       private List <Sale> sales_ = new List <Sale> ();
-        public void AddSale(Sale sale)
+       private List <Sale> Sales;
+
+        public SaleHistory()
         {
-            sales_.Add (sale);
+            Sales = new List<Sale>();
         }
+        public void Addsale(Sale Sale)
+        {
+            Sales.Add(Sale);
+        }
+
+        public void Sale_History()
+        {
+            double TotalAmount = 0;
+            foreach (var Sale in Sales)
+            {
+                var appliance = Sale.GetAppliances();
+
+                Console.WriteLine($"День {Sale.GetDay()}," +
+                    $" Наименование бытовой техники {appliance.GetName()}, " +
+                    $"Тип бытовой техники {appliance.GetType()}, " +
+                    $"Цена {appliance.GetPrice()}," +
+                    $"Количество {appliance.GetQuantity()}," +
+                    $"Прибыль {appliance.GetPrice() * appliance.GetQuantity()}");
+            }
+            if (TotalAmount != 0) 
+            {
+                Console.WriteLine($"Вся прибыль {TotalAmount}");
+            }
+            if (TotalAmount == 0)
+            {
+                Console.WriteLine("Не совершалось продаж в данный день");
+            }
+            Console.WriteLine($"Вся прибыль {TotalAmount}");
+        }
+       
     }
 }
 
