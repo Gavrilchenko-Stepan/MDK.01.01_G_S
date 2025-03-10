ac4e3c8cb88f407dc114211a7ff956aefb0f6823
Gavrilchenko_Stepan <@DESKTOP-DHH24NC>
2025-03-10 22:52:34 +0300

    Добавил класс Player(игрок) и описал его конструктор

--- ЛР-1/Лабораторная работа №1 Футбол/Football_LR1/Football_LR1/Program.cs
+++ ЛР-1/Лабораторная работа №1 Футбол/Football_LR1/Football_LR1/Program.cs
@@ -6,8 +6,20 @@ using System.Threading.Tasks;
 
 namespace Football_LR1
 {
+    public class Player
+    {
+        public string Name { get; set; }
+        public int Goals { get; set; }
+
+        public Player(string name, int goals)
+        {
+            Name = name;
+            Goals = goals;
+        }
+    }
     class Program
     {
+
         static void Main(string[] args)
         {
         }
