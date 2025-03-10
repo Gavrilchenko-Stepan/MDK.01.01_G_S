a8cc571094566f24bd7cbab822283a220f74b359
Gavrilchenko_Stepan <@DESKTOP-DHH24NC>
2025-03-10 22:56:05 +0300

    Добавил в список несколько команд и игроков, которые в них входят.

--- ЛР-1/Лабораторная работа №1 Футбол/Football_LR1/Football_LR1/Program.cs
+++ ЛР-1/Лабораторная работа №1 Футбол/Football_LR1/Football_LR1/Program.cs
@@ -31,11 +31,23 @@ namespace Football_LR1
             Players = players;
         }
     }
+
     class Program
     {
 
         static void Main(string[] args)
         {
+            List<Team> teams = new List<Team>
+        {
+        new Team("Торжок FC", 15, new List<Player> { new Player("Игрок1", 10), new Player("Игрок2", 5), new Player("Игрок3", 2) }),
+        new Team("Спартак", 20, new List<Player> { new Player("Игрок4", 8), new Player("Игрок5", 7), new Player("Игрок6", 3) }),
+        new Team("Локомотив", 18, new List<Player> { new Player("Игрок7", 12), new Player("Игрок8", 5), new Player("Игрок9", 1) }),
+        new Team("Зенит", 22, new List<Player> { new Player("Игрок10", 9), new Player("Игрок11", 6), new Player("Игрок12", 4) }),
+        new Team("Динамо", 10, new List<Player> { new Player("Игрок13", 1), new Player("Игрок14", 0), new Player("Игрок15", 0) }),
+        new Team("Кубань", 12, new List<Player> { new Player("Игрок16", 3), new Player("Игрок17", 5), new Player("Игрок18", 3) })
+        };
+
+
         }
     }
 }
