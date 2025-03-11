d9557d6647a40c832290adc01142a1759bd6e6c9
Gavrilchenko-Stepan <atom47228@gmail.com>
2025-03-11 10:45:19 +0300

    Описали модель College

---
+++ MyTreeView/My_TreeView/MyLibrary/Models/College.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace MyLibrary.Models
+{
+   public class College
+    {
+        public string Name { get; set; } //имя узла
+
+        public List<College> Children { get; } // список дочерних узлов
+
+        public College(string name)
+        {
+            Name = name;
+            Children = new List<College>();
+        }
+
+        public College AddChildNode(string text)
+        {
+            College node = new College(text);
+            Children.Add(node);
+
+            return node;
+        }
+    }
+}
---
+++ MyTreeView/My_TreeView/My_TreeView/MainForm.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Windows.Forms;
+using MyLibrary.Models;
+
+namespace My_TreeView
+{
+    public partial class MainForm: Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+
+        }
+    }
+}
