48049df66d8eafcffb9dd796f5c2d69de3e816ed
Gavrilchenko-Stepan <atom47228@gmail.com>
2025-03-25 13:22:56 +0300

    Не доделал

---
+++ MyVKWinFormsTets/MyVKWinFormsTest/MyVKWinFormsTest/MySqlUsersReader.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.PerformanceData;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+using MySql.Data.MySqlClient;
+
+namespace MyVKWinFormsTest
+{
+    class MySqlUsersReader
+    {
+        public List<User> ReadUsers()
+        {
+            List<User> result = new List<User>();
+
+            MySqlConnection conn;
+            string myConnectionString = "server=127.0.0.1;unid=root;database=myvknetwork;";
+            try
+            {
+                conn = new MySql.Data.MySqlClient.MySqlConnection(myConnectionString);
+                conn.Open();
+
+                const string query = "SELECT login, password, name, surname, data_birth FROM users";
+                MySqlCommand command = new MySqlCommand(query, conn);
+                using(MySqlDataReader reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        Console.WriteLine("User Login: " + reader["login"] + " | Password: " + reader["password"] + " | Name: " + reader["name"] + " | Surname: " + reader["surname"]);
+                    }
+                }
+            }
+            catch(MySqlException ex)
+            {
+                MessageBox.Show(ex.Message);
+                return result;
+            }
+        }
+        
+    }
+}
---
+++ MyVKWinFormsTets/MyVKWinFormsTest/MyVKWinFormsTest/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyVKWinFormsTest
+{
+    class User
+    {
+        public string login;
+        public string name { get; set; }
+        public string surname { get; set; }
+        public string password { get; set; }
+    }
+}
